7209ea95bd496fc6163b557a5f3bfb1a9588e744
Hack <hackrm12@deacnet.wfu.edu>
2017-02-17 14:58:43 -0500

    My first version of the CG app

---
+++ server.js
@@ -0,0 +1,21 @@
+var express = require('express'); //loading package express
+var httpModule = require('http'); //loading package http
+
+var app = express(); //create an express app (to hanle html stuff)
+var http = httpModule.Server(app);
+
+app.use(express.static('assets')); //any assets will be found in the folder assets (pics and such)
+
+function responder(req, res){
+  res.sendFile(__dirname + '/index.html'); //sending a file to the user's browser
+  console.log('got a request'); //print message in the server side console
+};
+
+app.get('/', responder); //get request to / is given to responder function
+
+function portListener(){
+  console.log('Listning on localhoset ' + port);
+};
+
+var port = process.env.PORT || 3000;
+http.listen(port, portListener);
